78f32240a612589b477d7ff9ad3ee339b6a3e92e
nut2567 <nutmakchine.n@gmail.com>
2025-05-06 17:05:51 +0700

    NextConfig  puck

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  collections: {
+    page: {
+      label: "Pages",
+      fields: {
+        title: {
+          type: "text",
+          label: "Title",
+        },
+        content: {
+          type: "textarea",
+          label: "Content",
+        },
+        seo: {
+          type: "object",
+          label: "SEO",
+          fields: {
+            title: { type: "text", label: "SEO Title" },
+            description: { type: "text", label: "Meta Description" },
+          },
+        },
+      },
+    },
+  },
 };
 
 export default nextConfig;
